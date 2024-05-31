cd082fd0368481d384d0c681e61e1fbb65e90ba7
UnaxOB1 <152311868+UnaxOB1@users.noreply.github.com>
2024-05-31 01:43:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from decimal import Decimal
+import math
+
+#1
+python_list = ['Yellow', 'Germany', 'Apple', 'Cat', 'Pepper']
+
+python_tuple = (2.50, 48 , [1, 2, 3], 'Orange', 'A long string')
+
+python_float = 59.99
+
+python_integer = 101
+
+python_decimal = Decimal(25.33)
+
+python_dictionary = {'name':'Chris', 'age': 30, 'password': 5633}
+
+#2
+print(math.ceil(python_float))
+
+#3
+print(math.sqrt(python_float))
+
+#4
+first_element = python_dictionary['name']
+print(first_element)
+
+#5
+second_element = python_tuple[1]
+print(second_element)
+
+#6
+longer_list = python_list + ['Last element']
+print(longer_list)
+
+#7
+python_list[0] = 'Red'
+print(python_list)
+
+#8
+sorted_list = sorted(python_list)
+print(sorted_list)
+
+#9
+new_tuple = python_tuple + ('More content',)
+print(new_tuple)
